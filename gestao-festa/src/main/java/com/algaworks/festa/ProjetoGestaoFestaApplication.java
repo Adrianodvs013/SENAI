af0bd7332c15b553c4fc3cc8465a8c0b35db38bb
Adriano Gomes <112133600+Adrianodvs013@users.noreply.github.com>
2023-05-16 03:35:28 -0300

    Primeiro Commit

---
+++ gestao-festa/src/main/java/com/algaworks/festa/ProjetoGestaoFestaApplication.java
@@ -0,0 +1,14 @@
+package com.algaworks.festa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoGestaoFestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoGestaoFestaApplication.class, args);
+	}
+
+}
+
